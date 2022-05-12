1693bdce45dca00eeba6e31aeb685ee987b15c24
Mihail Majukin <m.v.majukin@mail.ru>
2022-05-13 01:12:51 +0300

    исправить правки после второй проверки ревью

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,14 @@
 const buttonOpenPopupEdit = document.querySelector('.profile__edit-button');
-const popupEdit = document.querySelector('#popup_edit');
 const formElementEdit = document.querySelector('#popup__form_edit');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
 const profileTitle = document.querySelector('.profile__title');
 const profileSubtitle = document.querySelector('.profile__subtitle');
-const popup = document.querySelector('.popup');
-const buttonOpenPopupAdd = document.querySelector('.profile__add-button');
+const popupName = document.querySelector('.popup');
+const popupEdit = document.querySelector('#popup_edit');
 const popupAdd = document.querySelector('#popup_add');
+const popupImage = document.querySelector('#popup_image');
+const buttonOpenPopupAdd = document.querySelector('.profile__add-button');
 const buttonClosePopupEdit = document.querySelector('#popup__close-button_edit');
 const buttonClosePopupAdd = document.querySelector('#popup__close-button_add');
 
@@ -16,37 +17,38 @@ const popupFormAdd = document.querySelector('#popup__form_add');
 const placeInput = document.querySelector('.popup__input_type_place'); 
 const linkInput = document.querySelector('.popup__input_type_link');
 
-const popupImage = document.querySelector('#popup_image');
 const buttonClosePopupImage = document.querySelector('#popup__close-button_image');
 const popupImageCard = document.querySelector('.popup__image'); 
 const popupImageTitleCard = document.querySelector('.popup__image-title');
 
-nameInput.value = profileTitle.textContent;
-jobInput.value = profileSubtitle.textContent;
-
 // открывает попап
-const openPopup = (popup) => {
-  popup.classList.add('popup_opened');
+const openPopup = (popupName) => {
+  popupName.classList.add('popup_opened');
 };
-buttonOpenPopupEdit.addEventListener('click', ()=> openPopup(popupEdit));
-buttonOpenPopupAdd.addEventListener('click', ()=> openPopup(popupAdd)); 
+
+function handleOpenPopupEdit() { 
+  nameInput.value = profileTitle.textContent; 
+  jobInput.value = profileSubtitle.textContent;
+  openPopup(popupEdit);
+} 
+buttonOpenPopupEdit.addEventListener('click', handleOpenPopupEdit); 
+buttonOpenPopupAdd.addEventListener('click', ()=> openPopup(popupAdd));
 
 // закрывает попап
-const closePopup = (popup) => {
-  popup.classList.remove('popup_opened');
+const closePopup = (popupName) => {
+  popupName.classList.remove('popup_opened');
 };
 buttonClosePopupEdit.addEventListener('click', ()=> closePopup(popupEdit));
 buttonClosePopupAdd.addEventListener('click', ()=> closePopup(popupAdd));
-
-
+  
 // передает значение попап редактирования
-function formSubmitHandler(evt) {
+function handleSubmitForm(evt) {
   evt.preventDefault(); 
   profileTitle.textContent = nameInput.value;
   profileSubtitle.textContent = jobInput.value;
-  closePopupEdit();
+  closePopup(popupEdit);
 }
-formElementEdit.addEventListener('submit', formSubmitHandler); 
+formElementEdit.addEventListener('submit', handleSubmitForm); 
 
 const initialCards = [
   {
@@ -78,14 +80,13 @@ const initialCards = [
 // шаблоны
 const initialCardTemplate = document.querySelector("#element-template").content.querySelector('.element');
 
-
 // обработчики событий
 const handleSubmitAddInitialForm = (event) => {
   event.preventDefault();
   renderInitialCard({ link: linkInput.value, name: placeInput.value}); 
   linkInput.value = ''; 
   placeInput.value = ''; 
-  closePopupAdd();
+  closePopup(popupAdd);
 }
 
 const handleDeleteInitialCard = (event) => {
@@ -128,7 +129,7 @@ initialCards.forEach((initialData) => {
 popupFormAdd.addEventListener("submit", handleSubmitAddInitialForm);
 
 function handleOpenPopupImage(initialData) { // открывает попап картинки
-  popupImage.classList.add('popup_opened');
+  openPopup(popupImage);
   popupImageCard.src = initialData.link;
   popupImageCard.alt = initialData.name;
   popupImageTitleCard.textContent = initialData.name;
